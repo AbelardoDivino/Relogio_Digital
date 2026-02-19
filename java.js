19f7fea982ffed53982d4366025157569aafa18a
AbelardoDivino <aaaaaaaaaaasafafe@gmail.com>
2026-02-19 20:12:58 -0300

    update

--- java.js
+++ java.js
@@ -2,7 +2,23 @@ let dia = document.getElementById("dia")
 let hora = document.getElementById("hora")
 let minutos = document.getElementById("minutos")
 let segundos = document.getElementById("segundos")
-let res = document.getElementById("res")
 
-dia = new Date()
-res.innerHTML = dia
+
+// let agora  = new Date()
+
+// dia.innerHTML = agora.getDate()
+// hora.innerHTML = agora.getHours()
+// minutos.innerHTML = agora.getMinutes()
+// segundos.innerHTML = agora.getSeconds()
+
+function atualizarRelogio() {
+    let agora = new Date()
+
+    dia.innerHTML = agora.getDate()
+    hora.innerHTML = agora.getHours()
+    minutos.innerHTML = agora.getMinutes()
+    segundos.innerHTML = agora.getSeconds()
+}
+
+
+setInterval(atualizarRelogio, 1000)

73b8726ab74b9f26f454ba80f46ce192372e3617
AbelardoDivino <aaaaaaaaaaasafafe@gmail.com>
2026-02-19 20:03:21 -0300

    update

---
+++ java.js
@@ -0,0 +1,8 @@
+let dia = document.getElementById("dia")
+let hora = document.getElementById("hora")
+let minutos = document.getElementById("minutos")
+let segundos = document.getElementById("segundos")
+let res = document.getElementById("res")
+
+dia = new Date()
+res.innerHTML = dia
